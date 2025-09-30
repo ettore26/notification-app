8c0316286344ca741f4265e6a6a583f4b8e4c19f
ettore26 <ettoreivan26@gmail.com>
2025-09-30 02:28:05 -0400

    Added full working version

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -4,25 +4,70 @@
             [helix.dom :as d]
             ["react-dom/client" :as rdom]))
 
-;; define components using the `defnc` macro
-(defnc greeting
-  "A component which greets a user."
-  [{:keys [name]}]
-  ;; use helix.dom to create DOM elements
-  (d/div "Hello, " (d/strong name) "!"))
+(defnc notification-form []
+  (let [[form-data set-form-data]
+        (hooks/use-state {:user "" :category "" :message "" :channel "sms"})
+        [status set-status] (hooks/use-state nil)
+        handle-submit (fn []
+                        (set-status "Sending...")
+                        (let [payload (clj->js {:id (str (random-uuid))
+                                                :user (:user form-data)
+                                                :category (:category form-data)
+                                                :message (:message form-data)})
+                              url (str "/api/channel/" (:channel form-data))]
+                          (-> (js/fetch url
+                                        #js {:method "POST"
+                                             :headers #js {"Content-Type" "application/json"}
+                                             :body (js/JSON.stringify payload)})
+                              (.then #(.json %))
+                              (.then (fn [response]
+                                       (js/console.log "Response:" response)
+                                       (if (aget response "success")
+                                         (set-status "Success!")
+                                         (set-status (str "Failed: " (aget response "error"))))))
+                              (.catch (fn [error]
+                                        (js/console.error "Error:" error)
+                                        (set-status "Error: Request failed"))))))]
+    (d/div
+     (d/h2 "Send Notification")
+     (d/form
+      (d/div
+       (d/label "User: ")
+       (d/input {:type "text"
+                 :value (:user form-data)
+                 :on-change #(set-form-data assoc :user (.. % -target -value))}))
+      (d/div
+       (d/label "Channel: ")
+       (d/select {:value (:channel form-data)
+                  :on-change #(set-form-data assoc :channel (.. % -target -value))}
+                 (d/option {:value "sms"} "SMS")
+                 (d/option {:value "email"} "Email")
+                 (d/option {:value "push"} "Push")))
+      (d/div
+       (d/label "Category: ")
+       (d/select {:value (:category form-data)
+                  :on-change #(set-form-data assoc :category (.. % -target -value))}
+                 (d/option {:value ""} "Select a category")
+                 (d/option {:value "Sports"} "Sports")
+                 (d/option {:value "Finance"} "Finance")
+                 (d/option {:value "Movies"} "Movies")))
+      (d/div
+       (d/label "Message: ")
+       (d/textarea {:value (:message form-data)
+                    :on-change #(set-form-data assoc :message (.. % -target -value))}))
+      (d/button {:type "button"
+                 :on-click handle-submit}
+                "Send")
+      (when status
+        (d/div {:style {:margin-top "10px"
+                        :font-weight "bold"}}
+               status))))))
 
 (defnc app []
-  (let [[state set-state] (hooks/use-state {:name "Helix User"})]
-    (d/div
-     (d/h1 "Welcome!")
-      ;; create elements out of components
-     ($ greeting {:name (:name state)})
-     (d/input {:value (:name state)
-               :on-change #(set-state assoc :name (.. % -target -value))}))))
+  (d/div
+   ($ notification-form)))
 
-;; start your app with your favorite React renderer
 (defonce root (rdom/createRoot (js/document.getElementById "app")))
-; (.render root ($ app))
 
 (defn ^:export init []
   (js/console.log "Whizard!")
