ebe51db722a25dc8456a8df1739989fb90d6bb9f
ettore26 <ettoreivan26@gmail.com>
2025-09-29 20:48:46 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to app/core
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/app/core_test.clj
@@ -0,0 +1,7 @@
+(ns app.core-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [app.core :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
